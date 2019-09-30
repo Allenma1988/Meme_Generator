7c016641f988db5465a716cc8c2e1efe20ace0f9
allenma1988 <allenma1988@gmail.com>
2019-09-29 19:22:27 -0700

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Header(){
+	return (
+		<header>
+			<img src="logo192.png"
+			alt="problem?!"
+			/>
+			<p> Meme Generator </p>
+		</header>
+	)
+
+
+	
+}
+	
+export default Header
